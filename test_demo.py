252ae9439f0bd0410c02da56e80c708c275d41a7
Dilpreet <dilpreetj@Dilpreets-MacBook-Pro.local>
2023-12-19 18:36:16 -0500

    added dropdown test

--- test_demo.py
+++ test_demo.py
@@ -14,3 +14,18 @@ class TestDemoPage(BaseCase):
         # after state
         self.assert_attribute('#progressBar', 'value', '80')
 
+    def test_dropdown(self):
+        self.open("https://seleniumbase.io/demo_page")
+
+        # before state
+        self.assert_element('meter[value="0.25"]')
+
+        self.select_option_by_index('#mySelect', '2')
+        self.assert_element('meter[value="0.75"]')
+
+        self.select_option_by_text('#mySelect', 'Set to 100%')
+        self.assert_element('meter[value="1"]')
+
+        self.select_option_by_value('#mySelect', '50%')
+        self.assert_element('meter[value="0.5"]')
+
