5375a6d7a8df174a5dacdfaa99439147a0b4349e
Dilpreet <dilpreetj@Dilpreets-MacBook-Pro.local>
2023-12-24 19:40:58 -0500

    Added hooks in test_home

--- specs/test_home.py
+++ specs/test_home.py
@@ -3,29 +3,32 @@ from pages.home_page import HomePage
 
 
 class TestHomePage(BaseCase):
+    
+    def setUp(self, masterqa_mode=False):
+        super().setUp() # call the setup of the parent class if needed
+        self.homepage = HomePage(self)
+        self.homepage.open()
+
+        print('Login')
+        
+    def tearDown(self):
+        print('Log Out')
+        super().tearDown()
+
     def test_verify_page_title_and_url(self):
-        homepage = HomePage(self)
-
-        # open home page
-        homepage.open()
-
         # assert url and title contains SDET Unicorns
         self.assert_url_contains("sdetunicorns")
         self.assert_title_contains("SDET Unicorns")
 
+        print('TEST')
+
     def test_search_flow(self):
-        homepage = HomePage(self)
-
-        homepage.open()
-
-        homepage.search_for_item("Lenovo")
+        self.homepage.search_for_item("Lenovo")
 
         # assert the text is visible
-        self.assert_text_visible("Showing Results for Apple")
+        self.assert_text_visible("Showing Results for Lenovo")
 
     def test_search_flow_with_xpath(self):
-        self.open("https://practice-react.sdetunicorns.com/")
-
         # click on the search input field
         self.click("//button[@class='search-active']")
 
@@ -39,30 +42,22 @@ class TestHomePage(BaseCase):
         self.assert_text_visible("Showing Results for Lenovo")
 
     def test_nav_links(self):
-        homepage = HomePage(self)
-        homepage.open()
 
-        self.assert_text("Products", homepage.product_link)
+        self.assert_text("Products", self.homepage.product_link)
 
         expected_nav_text = ["Home", "Products", "About Us", "Contact", "Upload"]
 
-        homepage.verify_nav_links(expected_nav_text)
+        self.homepage.verify_nav_links(expected_nav_text)
 
     def test_click_about_link_and_verify_url(self):
-        homepage = HomePage(self)
-        homepage.open()
-
         # Click the "About" link
-        self.click(homepage.about_link)
+        self.click(self.homepage.about_link)
 
         # Assert URL
         self.assert_url_contains("about")
 
     def test_new_tab(self):
-        homepage = HomePage(self)
-        homepage.open()
-
-        self.click(homepage.copyright_link)
+        self.click(self.homepage.copyright_link)
 
         self.switch_to_tab(1)
 
