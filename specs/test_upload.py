038cfde0e4119ab988cbc275d71fa24711eeef14
Dilpreet <dilpreetj@Dilpreets-MacBook-Pro.local>
2023-12-24 21:53:37 -0500

    Added get_image_path util

--- specs/test_upload.py
+++ specs/test_upload.py
@@ -2,6 +2,7 @@ import os.path
 
 from seleniumbase import BaseCase
 from pages.upload_page import UploadPage
+from utils.helper import get_image_path
 
 
 class TestUploadPage(BaseCase):
@@ -11,9 +12,7 @@ class TestUploadPage(BaseCase):
         upload_page.open()
 
         # provide the file path
-        file_path = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '..', 'data', 'sb_logo.png')
-        )
+        file_path = get_image_path('sb_logo.png')
 
         upload_page.upload_single_file(file_path)
 
@@ -26,12 +25,8 @@ class TestUploadPage(BaseCase):
         upload_page.open()
 
         # provide the file path
-        file_path_1 = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '..', 'data', 'sb_logo.png')
-        )
-        file_path_2 = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '..', 'data', 'Selenium_Logo.png')
-        )
+        file_path_1 = get_image_path('sb_logo.png')
+        file_path_2 = get_image_path('Selenium_Logo.png')
 
         upload_page.upload_multiple_files([file_path_1, file_path_2])
 
--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,6 @@
+import os
+
+
 def assert_list_text(base_case, list_selector, expected_text):
     """
     Verifies the text of each element in a list matches the expected text
@@ -10,3 +13,15 @@ def assert_list_text(base_case, list_selector, expected_text):
     """
     for i, text in enumerate(expected_text, start=1):
         base_case.assert_text(text, f'{list_selector}:nth-child({i})')
+
+
+def get_image_path(file_name):
+    """
+    Helper function to get the absolute path of an image file in the data folder.
+
+    :param file_name: Name of the file to retrieve the path for.
+    :return: Absolute path of the file.
+    """
+    return os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'data', file_name)
+    )
