94ba26777714b1c10fd3ea7f214bc59096182247
Dilpreet <dilpreetj@Dilpreets-MacBook-Pro.local>
2023-12-30 18:45:52 -0500

    Added BaseTest Class

--- specs/test_shop.py
+++ specs/test_shop.py
@@ -1,22 +1,10 @@
 import requests
 from seleniumbase import BaseCase
 from utils.helper import assert_list_text
+from specs.base_test import BaseTest
 
 
-class TestShopPage(BaseCase):
-
-    def update_bs_status(self, status):
-        session_id = self.driver.session_id
-        auth_credentials = ('dilpreetj_tsvpl4', 'CtEEf6v5zYySKBpdR4w6')
-        url = f"https://api.browserstack.com/automate/sessions/{session_id}.json"
-        payload = {"status": status}
-        requests.put(url, auth=auth_credentials, json=payload)
-
-    def tearDown(self):
-        status = "failed" if self._outcome.errors else "passed"
-        self.update_bs_status(status)
-        super().tearDown()
-
+class TestShopPage(BaseTest):
     def test_verify_categories_text(self):
         self.open("https://practice-react.sdetunicorns.com/shop-grid-standard")
 
