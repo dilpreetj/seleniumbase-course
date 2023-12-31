238fabfb2677611da0c5445df16a817b575b7d99
Dilpreet <dilpreetj@Dilpreets-MacBook-Pro.local>
2023-12-30 19:07:51 -0500

    Add missing basetest code

--- specs/base_test.py
+++ specs/base_test.py
@@ -1,12 +1,16 @@
 from seleniumbase import BaseCase
 import requests
+import os
+from config.default import BROWSERSTACK_BASE_URL
 
 
 class BaseTest(BaseCase):
     def update_bs_status(self, status):
         session_id = self.driver.session_id
-        auth_credentials = ('dilpreetj_tsvpl4', 'CtEEf6v5zYySKBpdR4w6')
-        url = f"https://api.browserstack.com/automate/sessions/{session_id}.json"
+        bs_username = os.environ.get('BROWSERSTACK_USERNAME')
+        bs_access_key = os.environ.get('BROWSERSTACK_ACCESS_KEY')
+        auth_credentials = (bs_username, bs_access_key)
+        url = f"{BROWSERSTACK_BASE_URL}/automate/sessions/{session_id}.json"
         payload = {"status": status}
         requests.put(url, auth=auth_credentials, json=payload)
 
